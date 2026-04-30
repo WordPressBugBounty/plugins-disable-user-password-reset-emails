27e092695226431436c56227541f14d82dfc533c
Sean Murphy <sean@iamseanmurphy.com>
2026-04-30 18:44:27 +0000

    2.0

--- disable-user-password-reset-emails/disable-user-password-reset-emails.php
+++ disable-user-password-reset-emails/disable-user-password-reset-emails.php
@@ -1,31 +1,24 @@
 <?php
 /**
-
  * @package Disable User Password Reset Admin Notifications
- 
  */
-
 /*
-
 Plugin Name: Disable User Password Reset Admin Notifications
 Plugin URI: https://mindspikedesign.com/
 Description: Disable admin email notifications when a user changes their password. Simply activate the plugin and you will no longer receive a email notification when a user resets their password.
-Version: 1.9
+Version: 2.0
 Author: Chris Cook
 Author URI: https://chris-cook.net/
-Tested up to: 6.8.1
+Tested up to: 6.9.4
 License: GPL v3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: disable-user-password-reset-emails
-
 */
-
 if ( ! function_exists( 'wp_password_change_notification' ) ) {
-    function wp_password_change_notification( $user ) {
-        return;
-    }
+	function wp_password_change_notification( $user ) {
+		return;
+	}
 }
-
 
 /**
  * Get the current time and set it as an option when the plugin is activated.
@@ -33,65 +26,50 @@ if ( ! function_exists( 'wp_password_change_notification' ) ) {
  * @return null
  */
 function winwar_set_activation_date() {
-
-	$now = strtotime( "now" );
-	add_option( 'myplugin_activation_date', $now );
-
+	// FIX #6: Use time() instead of strtotime("now")
+	add_option( 'myplugin_activation_date', time() );
 }
 register_activation_hook( __FILE__, 'winwar_set_activation_date' );
 
-
-
 /**
- * Check date on admin initiation and add to admin notice if it was over 10 days ago.
+ * Check date on admin initiation and add to admin notice if it was over 7 days ago.
  *
  * @return null
  */
 function winwar_check_installation_date() {
-
-	// Added Lines Start
-	$nobug = "";
-	$nobug = get_option('winwar_no_bug');
-
-	if (!$nobug) {
-	// Added Lines End
-
+	$nobug = get_option( 'winwar_no_bug' );
+	// FIX #5: Removed redundant $nobug = "" initialisation
+	if ( ! $nobug ) {
 		$install_date = get_option( 'myplugin_activation_date' );
-		$past_date    = strtotime( '+7 days' );
-
-		if ( $past_date >= $install_date ) {
-
+		// FIX #1: Correctly check whether 7 days have elapsed since install
+		$past_date = strtotime( '+7 days', $install_date );
+		if ( time() >= $past_date ) {
 			add_action( 'admin_notices', 'winwar_display_admin_notice' );
-
 		}
-
-	// Added Lines Start
 	}
-	// Added Lines End
 }
 add_action( 'admin_init', 'winwar_check_installation_date' );
 
-
-
 /**
- * Display Admin Notice, asking for a review
+ * Display Admin Notice, asking for a review.
  *
  * @return null
  */
 function winwar_display_admin_notice() {
-
-	// Review URL - Change to the URL of your plugin on WordPress.org
 	$reviewurl = 'https://wordpress.org/support/plugin/disable-user-password-reset-emails/reviews/';
-
-	$nobugurl = get_admin_url() . '?winwarnobug=1';
-
+	$nobugurl  = get_admin_url() . '?winwarnobug=1';
 	echo '<div class="updated"><p>';
-	printf( __( "You have been using the <strong>Disable User Password Reset Admin Notifications</strong> plugin for a week now, do you like it? If so, please leave us a review with your feedback! <br /><br /> <a href='%s' target='_blank'>Leave A Review</a>/<a href='%s'>Leave Me Alone</a>" ), $reviewurl, $nobugurl );
-	echo "</p></div>";
+	// FIX #4: Added text domain to __()
+	printf(
+		__(
+			'You have been using the <strong>Disable User Password Reset Admin Notifications</strong> plugin for a week now, do you like it? If so, please leave us a review with your feedback! <br /><br /> <a href="%s" target="_blank">Leave A Review</a> / <a href="%s">Leave Me Alone</a>',
+			'disable-user-password-reset-emails'
+		),
+		esc_url( $reviewurl ),
+		esc_url( $nobugurl )
+	);
+	echo '</p></div>';
 }
-
-
-
 
 /**
  * Set the plugin to no longer bug users if user asks not to be.
@@ -99,19 +77,15 @@ function winwar_display_admin_notice() {
  * @return null
  */
 function winwar_set_no_bug() {
-
-	$nobug = "";
-
-	if ( isset( $_GET['winwarnobug'] ) ) {
-		$nobug = esc_attr( $_GET['winwarnobug'] );
+	// FIX #3: Added capability check before honouring the GET parameter
+	if ( isset( $_GET['winwarnobug'] ) && current_user_can( 'manage_options' ) ) {
+		$nobug = absint( $_GET['winwarnobug'] );
+		// FIX #5: Removed redundant $nobug = "" initialisation
+		if ( 1 === $nobug ) {
+			// FIX #2: Use update_option() so the value is always written
+			update_option( 'winwar_no_bug', true );
+		}
 	}
-
-	if ( 1 == $nobug ) {
-
-		add_option( 'winwar_no_bug', TRUE );
-
-	}
-
-} add_action( 'admin_init', 'winwar_set_no_bug', 5 );
-
-?>
+}
+add_action( 'admin_init', 'winwar_set_no_bug', 5 );
+// FIX #7: Removed closing PHP tag to prevent "headers already sent" issues
--- disable-user-password-reset-emails/readme.txt
+++ disable-user-password-reset-emails/readme.txt
@@ -3,22 +3,13 @@
 Contributors: cccamuseme
 Tags: emails, admin, notifications
 Requires at least: 4.9
-Tested up to: 6.8.1
-Stable tag: 1.9
+Tested up to: 6.9.4
+Stable tag: 2.0
 Requires PHP: 7.4
 License: GPL v3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
-
-Disable admin email notifications when a user changes their password.
+Disable User Password Reset Emails
 
 == Description ==
 
 Disable admin email notifications when a user changes their password. Simply activate the plugin and you will no longer receive a email notification when a user resets their password.
-
-== Installation ==
-
-1. Download the plugin ZIP file.
-2. Go to your WordPress admin dashboard.
-3. Navigate to **Plugins > Add New**.
-4. Click **Upload Plugin** and choose the ZIP file.
-5. Click **Install Now**, then **Activate**.
